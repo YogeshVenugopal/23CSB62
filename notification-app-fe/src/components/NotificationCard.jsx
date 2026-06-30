451de4cee9859f992e3115b627a189087d029e3f
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-30 12:53:31 +0530

    Ui: change the ui of the card

--- notification-app-fe/src/components/NotificationCard.jsx
+++ notification-app-fe/src/components/NotificationCard.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export const NotificationCard = ({key, notification}) => {
+  const color = {
+    "Result": "text-red-400",
+    "Event": "text-blue-400",
+    "Placement": "text-green-400"
+  }
   return (
     <div className='w-full p-4 rounded-[24px] shadow-lg flex flex-col items-start'>
       <div className='flex w-full items-center justify-between'>
@@ -12,8 +17,9 @@ export const NotificationCard = ({key, notification}) => {
       
       <h1 className='font-bold'>{notification.Message}</h1>
       <p
+      className={`${color[notification.Type]}`}
       >
-        Type : {notification.Type}
+       <span className='text-black'>Type: </span>{notification.Type}
       </p>
     </div>
   )
